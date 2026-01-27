fa4290dd92e37034e72065250597861aa2b949fe
Alexander Azocar <yazocar@mercantilseguros.com>
2026-01-26 20:06:06 -0400

    fix: import MainLayout with correct case

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Layout
-import MainLayout from "@/components/template/mainLayout";
+import MainLayout from "@/components/template/MainLayout";
 
 // Páginas existentes
 import Home from "@/pages/home";
