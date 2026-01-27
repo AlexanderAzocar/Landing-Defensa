8888237b246c94d441365c69c5f1b6ac1f5721fe
Alexander Azocar <yazocar@mercantilseguros.com>
2026-01-26 20:12:55 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Layout
-import MainLayout from "@/components/template/MainLayout";
+import MainLayout from "@/components/ui/mainLayout";
 
 // Páginas existentes
 import Home from "@/pages/home";
